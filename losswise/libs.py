763649a448ad6ba79c1f0bd2013ab91f21c2654c
Nicolas D. Jimenez <nicodjimenez@gmail.com>
2017-10-17 11:51:58 -0400

    Smarter Keras integration: inspect for metrics, #1.

--- losswise/libs.py
+++ losswise/libs.py
@@ -3,30 +3,34 @@ from keras.callbacks import Callback
 
 
 class LosswiseKerasCallback(Callback):
-    def __init__(self, tag=None, max_iter=None, use_acc=False, use_val=False, data={}, git_model_path=None):
+    def __init__(self, tag=None, max_iter=None, data={}, git_model_path=None):
         self.session = Session(tag=tag, max_iter=max_iter, data=data, git_model_path=git_model_path)
-        self.use_acc = use_acc
-        self.use_val = use_val
-        self.graph_loss = self.session.graph('loss', kind='min')
-        self.graph_acc = None
         self.max_iter = max_iter
-        if use_acc:
-            self.graph_acc = self.session.graph('acc', kind='max')
+        self.graph_map = {}
         super(LosswiseKerasCallback, self).__init__()
     def on_train_begin(self, logs={}):
-        self.x = 0
-    def on_batch_end(self, batch, logs={}):
-        if self.use_val:
-            self.graph_loss.append(self.x, {'train_loss': logs.get('loss'),
-                                            'val_loss': logs.get('val_loss')})
-        else:
-            self.graph_loss.append(self.x, {'train_loss': logs.get('loss')})
-        if self.use_acc:
-            if self.use_val:
-                self.graph_acc.append(self.x, {'train_acc': logs.get('acc'),
-                                               'val_acc': logs.get('val_acc')})
+        self.metric_list = []
+        for metric in self.params['metrics']:
+            if not metric.startswith('val_'):
+                if metric not in self.metric_list:
+                    self.metric_list.append(metric)
+        for metric in self.metric_list:
+            if 'acc' in metric:
+                kind = 'max'
             else:
-                self.graph_acc.append(self.x, {'train_acc': logs.get('acc')})
+                kind = 'min'
+            self.graph_map[metric] = self.session.graph(metric, kind=kind)
+        self.x = 0
+    def on_epoch_end(self, epoch, logs={}):
+        for metric in self.metric_list:
+            metric_val = "val_" + metric
+            if metric_val in logs:
+                data = {metric_val: logs[metric_val]}
+                self.graph_map[metric].append(self.x, data)
+    def on_batch_end(self, batch, logs={}):
+        for metric in self.metric_list:
+            data = {metric: logs.get(metric)}
+            self.graph_map[metric].append(self.x, data)
         self.x += 1
     def on_train_end(self, logs={}):
         self.session.done()
