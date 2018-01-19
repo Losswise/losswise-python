b2480f9af312ffdcbd90fabcd22e7d65795b7700
Nicolas D. Jimenez <nicodjimenez@gmail.com>
2018-01-18 16:14:47 -0800

    Python3 bugfix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='losswise',
-    version='1.03',
+    version='1.04',
     description='Official Losswise library for Python',
     long_description=open('README.rst').read(),
     url='https://losswise.com',
