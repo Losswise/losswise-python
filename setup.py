eb341b089b1536efd1b78e454a6f4155e13808b6
Nicolas D. Jimenez <nicodjimenez@gmail.com>
2019-06-17 14:15:25 -0700

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='losswise',
-    version='3.7',
+    version='3.8',
     description='Official Losswise library for Python',
     long_description=open('README.rst').read(),
     url='https://losswise.com',
